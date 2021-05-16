81dfad5d7a8427d17f6b097106fec66f40d364c8
Dmitry Kutuzov <dimkut@mail.rug>
2021-05-16 19:16:50 +0400

    promise

---
+++ currency/js/script.js
@@ -0,0 +1,28 @@
+
+let inputRub = document.getElementById('rub'),
+    inputUsd = document.getElementById('usd');
+
+inputRub.addEventListener('input', () => {
+    let promise = new Promise(function(resolve,reject){
+        let request = new XMLHttpRequest();
+
+        request.open('GET', 'js/current.json');
+        request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+        request.send();
+        
+        request.onreadystatechange = function() {
+            if (request.status === 200) {
+                let data = JSON.parse(request.response);
+
+                resolve(data);
+            } else {
+                // inputUsd.value = "Что-то пошло не так!";
+                reject();
+            }
+        };
+    });
+    promise .then((result)=>{
+                    inputUsd.value = inputRub.value / result.usd;
+                })
+            .catch(()=>console.log( "Что-то пошло не так!"));
+});
